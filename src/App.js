531fe8d590f2fb01715cdba698a9a09fc0c16983
Mingyu3 <aeneascon@naver.com>
2021-08-16 23:41:27 +0900

    init again

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
 import './App.css';
+import { useRef, useState } from 'react';
+
+function TodoItem({ todoItem, toggleTodo, removeTodo }) {
+	const { id, todo, active } = todoItem;
+	return (
+		<div>
+			<span
+				onClick={() => toggleTodo(id)}
+				style={{ textDecoration: active && 'line-through' }}>
+				{todo}
+			</span>
+			<button onClick={() => removeTodo(id)} style={{ marginLeft: '5px' }}>
+				X
+			</button>
+		</div>
+	);
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [newTodo, setNewTodo] = useState('');
+	const [todoList, setTodoList] = useState([
+		{
+			id: 0,
+			todo: '밥먹기',
+			active: false,
+		},
+		{
+			id: 1,
+			todo: '공부하기',
+			active: false,
+		},
+	]);
+	const inputRef = useRef();
+	const nextId = useRef(2);
+
+	function addTodo() {
+		setTodoList([
+			...todoList,
+			{ id: nextId.current, todo: newTodo, active: false },
+		]);
+		nextId.current++;
+		setNewTodo('');
+		inputRef.current.focus();
+	}
+
+	function removeTodo(id) {
+		setTodoList(todoList.filter((todo) => todo.id !== id));
+	}
+
+	function toggleTodo(id) {
+		setTodoList(
+			todoList.map((todo) =>
+				todo.id === id ? { ...todo, active: !todo.active } : todo,
+			),
+		);
+	}
+
+	function changeInputData(e) {
+		setNewTodo(e.target.value);
+	}
+
+	return (
+		<div>
+			<input onChange={changeInputData} value={newTodo} ref={inputRef} />
+			<button onClick={addTodo}>추가</button>
+			<div>
+				{todoList.map((todoItem) => (
+					<TodoItem
+						key={todoItem.id}
+						todoItem={todoItem}
+						toggleTodo={toggleTodo}
+						removeTodo={removeTodo}
+					/>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default App;
